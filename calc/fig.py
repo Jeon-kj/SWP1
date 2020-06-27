43fc0d9efaa44beafe6ec9af2f549bf4cdc2a67c
jeon-kj <jeonkj0413@naver.com>
2020-06-28 00:25:20 +0900

    Add WSGI files (fig.py, template.py, fig.conf) #4

---
+++ calc/fig.py
@@ -0,0 +1,24 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+        d = parse_qs(environ['QUERY_STRING'])
+        if environ['QUERY_STRING'] != '' :
+                a = d.get('a', [''])[0]
+                b = d.get('b', [''])[0]
+                a, b = int(a), int(b)
+                response_body = html % {
+                        'sum':a+b,
+                        'mul':a*b,
+                }
+
+	else :
+		response_body = html
+
+	status = '200 OK'
+	response_headers = [
+		('Content-Type', 'text/html'),
+		('Conetent-Length', str(len(response_body)))
+	]
+	start_response(status, response_headers)
+	return [response_body]
---
+++ calc/template.py
@@ -0,0 +1,14 @@
+html = """
+<html>
+<body>
+        <form method="get" action="">
+                a = <input type="text" name="a">
+                b = <input type="text" name="b">
+                <input type="submit">
+        </form>
+                a + b = : %(sum)s</br>
+                a * b = : %(mul)s</br>
+</body>
+</html>
+"""
+
