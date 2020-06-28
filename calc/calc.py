69a669f6d57c63ec578583e257528261d3da1c85
jeon-kj <jeonkj0413@naver.com>
2020-06-28 16:42:33 +0900

    modify *.py (\t\t -> \t)

--- calc/calc.py
+++ calc/calc.py
@@ -2,35 +2,36 @@ from cgi import parse_qs
 from calc_template import html
 
 def application(environ, start_response):
-        d = parse_qs(environ['QUERY_STRING'])
-        a = d.get('a', [''])[0]
-        b = d.get('b', [''])[0]
-        if environ['QUERY_STRING'] != '' :
-                if a.isdigit() == False or b.isdigit() == False :
-                        response_body = html % {
-                                'sum':-1,
-                                'mul':-1,
-                        }
-
-                else :
-                        a, b = int(a), int(b)
-                        response_body = html % {
-                                'sum':a+b,
-                                'mul':a*b,
-                        }
-
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
+    if environ['QUERY_STRING'] != '' :
+        if a.isdigit() == False or b.isdigit() == False :
+            response_body = html % {
+                    'sum':-1,
+                    'mul':-1,
+            }
+            
         else :
-                response_body = html % {
-                        'sum':0,
-                        'mul':0,
-                }
+
+            a, b = int(a), int(b)
+            response_body = html % {
+                    'sum':a+b,
+                    'mul':a*b,
+            }
+
+    else :
+            response_body = html % {
+                    'sum':0,
+                    'mul':0,
+            }
 
 
-        status = '200 OK'
-        response_headers = [
-                ('Content-Type', 'text/html'),
-                ('Conetent-Length', str(len(response_body)))
-        ]
-        start_response(status, response_headers)
-        return [response_body]
+    status = '200 OK'
+    response_headers = [
+            ('Content-Type', 'text/html'),
+            ('Conetent-Length', str(len(response_body)))
+    ]
+    start_response(status, response_headers)
+    return [response_body]
 
--- calc/calc_template.py
+++ calc/calc_template.py
@@ -1,13 +1,13 @@
 html = """
 <html>
 <body>
-        <form method="get" action="">
-                a = <input type="number" name="a">
-                b = <input type="number" name="b">
-                <input type="submit">
-        </form>
-                a + b = : %(sum)s</br>
-                a * b = : %(mul)s</br>
+    <form method="get" action="">
+        a = <input type="number" name="a">
+        b = <input type="number" name="b">
+        <input type="submit">
+    </form>
+        a + b = : %(sum)s</br>
+        a * b = : %(mul)s</br>
 </body>
 </html>
 """
